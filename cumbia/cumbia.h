ee000bdf9e4e5582a4066f554cf3ef8ebf067e9d
Fede <federicobadaloni@hotmail.com>
2016-05-17 15:22:34 -0300

    a tiene los el ej 1 de la plancha 3 i

---
+++ cumbia/cumbia.h
@@ -0,0 +1,4 @@
+void readStrFromUsr(int usrAddr, char *outStr);
+void readBuffFromUsr(int usrAddr, char *outBuff, int byteCount);
+void writeStrToUsr(char *str, int usrAddr);
+void writeBuffToUsr(char *str, int usrAddr, int byteCount);
