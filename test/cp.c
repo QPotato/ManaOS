b45540835498bf6f21baeb987e80dfcca16e5456
Fede <federicobadaloni@hotmail.com>
2017-05-24 13:26:20 -0300

    Hice cp y fixie algo en la consola

---
+++ test/cp.c
@@ -0,0 +1,26 @@
+#include "syscall.h"
+#define MAX_FILENAME 128
+#define BLOCK_BYTES 256
+
+int main()
+{
+    int argc = getArgc();
+    if(argc < 3)
+        return 1;
+    char origin[MAX_FILENAME], destination[MAX_FILENAME];
+    getArg(1, origin);
+    getArg(2, destination);
+    
+    Create(destination);
+    OpenFileId o = Open(origin), d = Open(destination);
+    char block[BLOCK_BYTES];
+    int read = BLOCK_BYTES;
+    Write("Hola\n", 5, ConsoleOutput);
+    while(read == BLOCK_BYTES)
+    {
+        read = Read(block, BLOCK_BYTES, o);
+        Write(block, read, d);
+    }
+    Close(o); Close(d);
+    return 0;
+}
